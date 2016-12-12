2d65e7c0f41d1bfde96227ab61d07829758efd8a
github username <github email address>
2016-12-12 10:16:53 -0500

    done

--- lib/song.rb
+++ lib/song.rb
@@ -8,6 +8,7 @@ class Song
     self.to_s.downcase.pluralize
   end
 
+
   def self.column_names
     DB[:conn].results_as_hash = true
 
@@ -21,9 +22,11 @@ class Song
     column_names.compact
   end
 
+
   self.column_names.each do |col_name|
     attr_accessor col_name.to_sym
   end
+
 
   def initialize(options={})
     options.each do |property, value|
@@ -31,15 +34,18 @@ class Song
     end
   end
 
+
   def save
     sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
     DB[:conn].execute(sql)
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
   end
 
+
   def table_name_for_insert
     self.class.table_name
   end
+
 
   def values_for_insert
     values = []
@@ -49,16 +55,16 @@ class Song
     values.join(", ")
   end
 
+
   def col_names_for_insert
     self.class.column_names.delete_if {|col| col == "id"}.join(", ")
   end
+
 
   def self.find_by_name(name)
     sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
     DB[:conn].execute(sql)
   end
 
+
 end
-
-
-
